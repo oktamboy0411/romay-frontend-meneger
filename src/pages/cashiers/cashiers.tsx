6ad9a4c8df78f3c8563e2d95883f149290d3d2b2
qulsoatovoktam <129328648+oktamboy0411@users.noreply.github.com>
2025-09-12 14:58:29 +0500

    Update cashiers.tsx

--- src/pages/cashiers/cashiers.tsx
+++ src/pages/cashiers/cashiers.tsx
@@ -1,91 +1,71 @@
 import { Link } from 'react-router-dom'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import { TableSkeleton } from '../../components/ui/table-skeleton'
-import { AlertCircle, Search } from 'lucide-react'
+import { AlertCircle, Plus, Search } from 'lucide-react'
 import { useState } from 'react'
 import AddClientDialog from './AddCashierDialog'
-import { useGetAllBranchesQuery } from '@/store/branch/branch.api'
 import { Input } from '@/components/ui/input'
 import { PaginationComponent } from '@/components/pagination'
 import { Button } from '@/components/ui/button'
 import { useGetCashiersQuery } from '@/store/cashiers/cashiers'
 
 function Cashiers() {
-  const [search, setSearch] = useState('')
-  const [branch, setBranch] = useState('all')
+  const [searchFilter, setSearchFilter] = useState('')
+  const [roleFilter, setRoleFilter] = useState('sale_cashier')
   const [page, setPage] = useState(1)
   const [open, setOpen] = useState(false)
 
-  // Query parametrlari
   const queryParams = {
-    search,
-    ...(branch !== 'all' && { branch_id: branch }),
+    search: searchFilter,
+    ...(roleFilter !== 'all' && { role: roleFilter }),
     page,
   }
 
-  // Mijozlar API
   const {
     data: { data: clientsData = [], pagination } = {},
     isLoading,
     isError,
   } = useGetCashiersQuery(queryParams)
 
-  const {
-    data: { data: branchesData = [] } = {},
-    isLoading: isLoadingBranches,
-    isError: isErrorBranches,
-  } = useGetAllBranchesQuery({})
-
   return (
     <div className="space-y-6">
-      {/* Header */}
-      <div className="flex justify-between items-center">
-        <h1 className="text-[30px] font-semibold text-[#09090B]">Kasserlar</h1>
-        <div className="flex gap-3">
-          {/* Search */}
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-3 justify-between items-center">
+          <h1 className="text-[30px] font-semibold text-[#09090B]">
+            Kasserlar
+          </h1>
+
+          <div className="flex justify-center items-center gap-1 border border-gray-300 rounded-md p-1">
+            <Button
+              onClick={() => setRoleFilter('sale_cashier')}
+              variant={roleFilter === 'sale_cashier' ? 'default' : 'ghost'}
+            >
+              Sotuv kassasi
+            </Button>
+            <Button
+              onClick={() => setRoleFilter('rent_cashier')}
+              variant={roleFilter === 'rent_cashier' ? 'default' : 'ghost'}
+            >
+              Servis kassasi
+            </Button>
+          </div>
+        </div>
+        <div className="flex gap-3 justify-between items-center">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400" />
             <Input
               className="pl-9 w-[300px]"
-              placeholder="mijozni izlash"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              placeholder="kasserni izlash"
+              value={searchFilter}
+              onChange={(e) => setSearchFilter(e.target.value)}
             />
           </div>
 
-          {/* Branch select */}
-          <Select value={branch} onValueChange={setBranch}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Barcha filiallar" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="all">Barchasi</SelectItem>
-              {isLoadingBranches ? (
-                <SelectItem disabled value="loading">
-                  Yuklanmoqda...
-                </SelectItem>
-              ) : isErrorBranches ? (
-                <SelectItem disabled value="error">
-                  Xatolik yuz berdi
-                </SelectItem>
-              ) : (
-                branchesData.map((branch) => (
-                  <SelectItem key={branch._id} value={branch._id}>
-                    {branch.name}
-                  </SelectItem>
-                ))
-              )}
-            </SelectContent>
-          </Select>
-
-          {/* Add new cashier */}
-          <Button onClick={() => setOpen(true)} className="py-5">
+          <Button
+            variant={'outline'}
+            onClick={() => setOpen(true)}
+            className="py-5"
+          >
+            <Plus className="h-4 w-4" />
             Yangi kasser qo'shish
           </Button>
         </div>
